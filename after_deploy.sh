1e731f40293b2e32e9e753e4df50506c6d8fd2a8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-03 05:46:28 +0000

    Deploy Enterprise-connect/certifactory to github.com/Enterprise-connect/certifactory.git:gh-pages

--- after_deploy.sh
+++ after_deploy.sh
@@ -2,5 +2,5 @@
 git config user.email "EC.Bot@ge.com"
 git config user.name "EC Bot"
 git add cert-list/.
-git commit -m 'licensed to ${CSR_ID} [skip ci]'
+git commit -m 'licensed to '$CSR_ID' [skip ci]'
 git push https://${GITHUB_TOKEN}@github.com/Enterprise-connect/certifactory.git HEAD:beta
