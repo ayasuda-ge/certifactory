c6b2f47c4f006c6fb6e0c8b6d1504a908074f52a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-05 13:54:12 +0000

    Deploy Enterprise-connect/certifactory to github.com/Enterprise-connect/certifactory.git:gh-pages

--- before_deploy.sh
+++ before_deploy.sh
@@ -3,11 +3,11 @@
 export REQ_EMAIL=$(openssl req -in csr-list/$CSR_ID.csr -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
 echo ********* Requester Email: $REQ_EMAIL
 cd cert-list/beta/
-bash <(curl -s https://enterprise-connect.github.io/oci/k8s/conf.txt) -ver
+source <(wget -O - https://enterprise-connect.github.io/sdk/scripts/agt/v1.1beta.linux64.txt) -ver
 echo $CA_PKEY | base64 --decode > ca.key
 echo $CA_CERT | base64 --decode > ca.cer
-export EC_PPS=$(./agent_linux_sys -hsh -dat $CA_PPRS)
-./agent_linux_sys -sgn <<MSG
+export EC_PPS=$(agent -hsh -dat $CA_PPRS)
+agent -sgn <<MSG
 ca.key
 365
 DEVELOPER
@@ -17,6 +17,6 @@ Seat_x1
 no 
 ca.cer
 MSG
-rm ca.key ca.cer agent_linux_sys
+rm ca.key ca.cer
 ls -al ./ && ls -al ./../..
 cd -
