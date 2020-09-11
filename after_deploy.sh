431bd6f9cb871b08780dca4d4c06fe3e1dd817f0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-11 18:51:52 +0000

    Deploy EC-Release/certifactory to github.com/EC-Release/certifactory.git:disty

--- after_deploy.sh
+++ after_deploy.sh
@@ -3,4 +3,4 @@ git config user.email "EC.Bot@ge.com"
 git config user.name "EC Bot"
 git add cert-list/.
 git commit -m 'licensed to '$CSR_ID' [skip ci]'
-git push https://${GITHUB_TOKEN}@github.com/Enterprise-connect/certifactory.git HEAD:beta
+git push https://${GITHUB_TOKEN}@github.com/EC-Release/certifactory.git HEAD:beta
--- before_deploy.sh
+++ before_deploy.sh
@@ -3,7 +3,7 @@
 export REQ_EMAIL=$(openssl req -in csr-list/$CSR_ID.csr -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
 echo ********* Requester Email: $REQ_EMAIL
 cd cert-list/beta/
-source <(wget -O - https://enterprise-connect.github.io/sdk/scripts/agt/v1.1beta.linux64.txt) -ver
+source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.1beta.linux64.txt) -ver
 echo $CA_PKEY | base64 --decode > ca.key
 echo $CA_CERT | base64 --decode > ca.cer
 export EC_PPS=$(agent -hsh -dat $CA_PPRS)
