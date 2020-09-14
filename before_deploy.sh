f6932d3c4dd4ee8bda4bd288cf26a72f066f8b69
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-14 02:28:23 +0000

    Deploy EC-Release/certifactory to github.com/EC-Release/certifactory.git:disty

--- before_deploy.sh
+++ before_deploy.sh
@@ -3,10 +3,10 @@
 export REQ_EMAIL=$(openssl req -in csr-list/$CSR_ID.csr -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
 echo ********* Requester Email: $REQ_EMAIL
 cd cert-list/beta/
-source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.1beta.linux64.txt) -ver
+source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64.txt) -ver
 echo $CA_PKEY | base64 --decode > ca.key
 echo $CA_CERT | base64 --decode > ca.cer
-export EC_PPS=$(agent -hsh -dat $CA_PPRS)
+export EC_PPS=$CA_PPRS
 agent -sgn <<MSG
 ca.key
 365
