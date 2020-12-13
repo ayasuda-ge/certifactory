e35062492dd50882c5523213ba35d2bcc0a0b9a9
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-12 23:02:40 -0800

    update path [skip ci]

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,7 +2,7 @@
 #export CSR_FILE=$(ls -t csr-list/ | awk '{printf("%s",$0);exit}')
 export REQ_EMAIL=$(openssl req -in csr-list/$CSR_ID.csr -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
 echo ********* Requester Email: $REQ_EMAIL
-cd cert-list/beta/
+cd cert-list/release/
 source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2.linux64.txt) -ver
 echo $CA_PKEY | base64 --decode > ca.key
 echo $CA_CERT | base64 --decode > ca.cer
@@ -17,7 +17,7 @@ ca.key
 DEVELOPER
 EC_ECO
 Seat_x1
-./csr-list/${CSR_ID}.csr
+./../../csr-list/${CSR_ID}.csr
 no 
 ca.cer
 MSG
